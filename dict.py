23b8f95e74f88073ff1ab49d880c83dae3a41c4a
bulatfed <123536963+bulatfed@users.noreply.github.com>
2026-09-11 22:06:20 +0300

    dict practick

--- dict.py
+++ dict.py
@@ -121,6 +121,230 @@
 # if  a in d:
 #     print(d[a])
 # else:
-#     print(d.setdefault(a, "неизвестно"))
+#     print(d.4default(a, "неизвестно"))
 #     print(d)
 
+"----------------------------------------------------"
+
+ #Доступ и проверка
+#1
+# d = {"a": 1, "b": 2, "c": 3}                  
+# if "d" in d:
+#     print(True)
+# else:
+#     print(False)
+
+#2
+# d = {"name": "Иван", "age": 25}
+# a = input("Введите ключ " " " )
+
+# if a not in d:
+#     print("неизвестно")
+# else:
+#     print(d[a])
+
+#3
+# d = {"x": 1, "y": 2}
+# print(list(d.values()))
+
+#4
+# d = {}
+
+# if d == {}:
+#     print(True)
+# else:
+#     print(False)
+
+#5
+# d = {"a": 1, "b": 2, "c": 3}
+# print(min(d.values()), max(d.values()))
+
+#Добавление и удаление
+
+#6
+# d = {"a": 1, "b": 2}
+# d["c"] = 3
+# print(d) 
+
+#7
+# d = {"name": "Иван", "age": 25}
+# d["age"] = 26
+# print(d)
+
+#8
+# d = {"a": 1, "b": 2, "c": 3}
+# del d["b"]
+# print(d)
+
+#9
+# d = {"name": "Иван"}
+# a = input("Введите ключ" " ")
+# if a in d:
+#     print(d[a])
+# else:
+#     d.setdefault(a, "неизвестно")
+#     print(d)
+
+#10
+# d = {"a": 10, "b": 20, "c": 30}
+# for i, a in d.items():
+#     print(f"{i}: {a}")
+
+#11
+# d = {"a": 1, "b": 2, "c": 3}
+# print(sum(d.values()))
+
+#12
+# d = "helloplkjklkuihuikgolllllllllllllllllo"
+# c = {}
+# for i in range(len(d)):
+#     if d[i] not in c:
+#         c[d[i]] = 1
+#     else:
+#         c[d[i]]+=1
+# print(c)
+
+#13
+# d = {"a": 5, "b": 12, "c": 7}
+# print(max(d.values()))
+
+# #14
+# d1 = {"a": 1, "b": 2}
+# d2 = {"b": 99, "c": 3}
+# # ожидаем {"a": 1, "b": 99, "c": 3}
+# df = d1
+# df.update(d2)
+# print(df)
+
+#15
+# a = [1, 3, 2, 3, 1, 3, 2, 1]
+# # ожидаем 3
+
+# d1 = {}
+# for i in a:
+#     if i not in d1:
+#         d1[i] = 1
+#     else:
+#         d1[i]+=1
+        
+# max = 0
+# max_num = None
+
+# for i in d1:
+#     if d1[i]>max:
+#         max = d1[i]
+#         max_num = i
+# print(max_num)
+
+
+#----------------------------------------------------------------------
+
+
+
+#1
+# a = [1, 2, 2, 3, 3, 3]
+# d = {}
+
+# for i in a:
+#     d[i] = d.get(i, 0) + 1
+# print(d)     
+
+#2
+# a = "abracadabra"
+# d = {}
+
+# for i in a:
+#     d[i] = d.get(i, 0) + 1
+# print(d)
+
+# #3
+# a = ["кот", "пес", "кот", "кот", "пёс"]
+# d = {}
+# for i in a:
+#     d[i] = d.get(i, 0) + 1
+# print(d)
+
+#4
+# a = [1, 1, 1, 2, 2, 3]
+# d = {}
+# for i in a:
+#     d[i] = d.get(i, 0) + 1
+
+# for k, v in d.items():
+#     if v > 1:
+#         print(k)
+
+#5
+# d = "hello world"
+# a = list(d)
+# bukv = {}
+# for i in a:
+#     if i == " ":
+#         continue    
+#     bukv[i] = bukv.get(i, 0) + 1 
+
+
+# print(bukv)
+
+#6
+# d = {"a": 3, "b": 7, "c": 5}
+# print(max(d, key=d.get)) 
+
+#7
+# d = {"a": 3, "b": 7, "c": 5}
+# print(min(d, key=d.get)) 
+
+#8
+# d = {"a": 3, "b": 7, "c": 5}
+
+# for k, v in d.items():
+#     if v > 4:
+#         print(k)
+
+#9
+# d = {"a": 3, "b": 7, "c": 5}
+# print(sum(d.values() ) / 2)
+
+#10
+# d = {"Иван": 4, "Оля": 5, "Петя": 3}
+# for k, v in d.items():
+#     if v >= 4:
+#         print(k)
+
+#11
+# a = ["кот", "собака", "дом"]
+# d = {}
+
+# d = {s: len(s) for s in a}
+# print(d)
+
+#12
+# a = [1, 2, 3, 4, 5]
+# d = {}
+
+# d = {s: s**2 for s in a}
+# print(d)
+
+#13
+# a = ["apple", "banana", "cherry"]
+# d = {s: s[0] for s in a}
+# print(d)
+
+#14
+# a = [1, 2, 2, 3, 3, 3]
+# s = {}
+# for i in a:
+#     s[i] = s.get(i, 0) + 1
+# print(s)
+
+#15
+# a = ["a", "b", "a", "c", "b", "a"]
+# chas = {}
+# for i in a:
+#     chas[i] = chas.get(i, 0) + 1
+# for i, v in chas.items():
+#     if v == 1:
+#         print(i)
+#     else:
+#         continue
+            
